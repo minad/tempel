5410821b3cf239c03649ecba88d7b733a331f7d7
Daniel Mendler <mail@daniel-mendler.de>
2022-12-21 20:46:34 +0100

    Fix for Emacs 29

--- tempel.el
+++ tempel.el
@@ -525,9 +525,12 @@ This is meant to be a source in `tempel-template-sources'."
 
 (defun tempel--field-at-point ()
   "Return the field overlay at point."
-  (cl-loop for ov in (overlays-in (max (point-min) (1- (point)))
-                                  (min (point-max) (1+ (point))))
-           thereis (and (overlay-get ov 'tempel--field) ov)))
+  (let ((start most-positive-fixnum) field)
+    (dolist (ov (overlays-in (max (point-min) (1- (point)))
+                             (min (point-max) (1+ (point)))))
+      (when (and (overlay-get ov 'tempel--field) (< (overlay-start ov) start))
+        (setq start (overlay-start ov) field ov)))
+    field))
 
 (defun tempel-kill ()
   "Kill the field contents."
