30def371d8282cfc60b1d90debd08f6240eba37c
Daniel Mendler <mail@daniel-mendler.de>
2023-01-19 06:41:05 +0100

    Add key-valid-p check

--- tempel.el
+++ tempel.el
@@ -700,7 +700,8 @@ If called interactively, select a template with `completing-read'."
 ;;;###autoload
 (defmacro tempel-key (key template-or-name &optional map)
   "Bind KEY to TEMPLATE-OR-NAME in MAP."
-  (keymap--check key)
+  (unless (key-valid-p key)
+    (error "Invalid key %s" key))
   `(define-key ,(or map 'global-map) ,(key-parse key)
      ,(if (consp template-or-name)
           `(lambda ()
