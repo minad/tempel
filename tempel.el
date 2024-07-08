51eff558b986aa80e816bb00fbaf310f03f017c0
Daniel Mendler <mail@daniel-mendler.de>
2024-07-08 23:20:25 +0200

    Bump Compat dependency

--- tempel.el
+++ tempel.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Daniel Mendler <mail@daniel-mendler.de>
 ;; Created: 2022
 ;; Version: 1.1
-;; Package-Requires: ((emacs "27.1") (compat "29.1.4.4"))
+;; Package-Requires: ((emacs "27.1") (compat "30"))
 ;; Homepage: https://github.com/minad/tempel
 ;; Keywords: abbrev, languages, tools, text
 
